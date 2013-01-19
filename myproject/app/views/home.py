957b52a6a255c3ab85270283f20deeb475556bc8
U Chandan <chandanchotu@gmail.com>
2013-01-19 02:51:53 -0500

    trivial login page created, with home view

--- myproject/app/views/home.py
+++ myproject/app/views/home.py
@@ -3,9 +3,9 @@ from django.template import TemplateDoesNotExist
 from django.views.generic.simple import direct_to_template
 
 
-def home(request, self):
+def home(request):
     try:
-        return direct_to_template(request, template="login.hmtl")
+        return direct_to_template(request, template="login.html")
     except TemplateDoesNotExist:
         raise Http404()
 
--- myproject/urls.py
+++ myproject/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-
+from app.views import home
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -22,5 +22,5 @@ urlpatterns = patterns('',
      url(r'^admin/', include(admin.site.urls)),
    
     # app urls 
-    url('^$', app.views.home, name='home'),       
+    url('^$', home.home, name='home'),       
 )
